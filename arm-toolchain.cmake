3482e99c2bfff0c46d28f2b9f5ff422e1fd91fb8
a-int <atunkin64@icloud.com>
2023-06-02 21:49:43 +0300

    replace the path to toolchain by the variable

--- arm-toolchain.cmake
+++ arm-toolchain.cmake
@@ -2,11 +2,14 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER /opt/homebrew/bin/arm-none-eabi-gcc CACHE INTERNAL "")
-set(CMAKE_CXX_COMPILER /opt/homebrew/bin/arm-none-eabi-g++ CACHE INTERNAL "")
-set(CMAKE_LINKER /opt/homebrew/bin/arm-none-eabi-ld CACHE INTERNAL "")
-set(CMAKE_AR /opt/homebrew/bin/arm-none-eabi-ar CACHE INTERNAL "")
-set(CMAKE_OBJCOPY /opt/homebrew/bin/arm-none-eabi-objcopy
+set(arm-toolchain-path "" CACHE PATH "The path to the arm toolcahin folder")
+
+
+set(CMAKE_C_COMPILER ${arm-toolchain-path}/bin/arm-none-eabi-gcc CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER ${arm-toolchain-path}/bin/arm-none-eabi-g++ CACHE INTERNAL "")
+set(CMAKE_LINKER ${arm-toolchain-path}/bin/arm-none-eabi-ld CACHE INTERNAL "")
+set(CMAKE_AR ${arm-toolchain-path}/bin/arm-none-eabi-ar CACHE INTERNAL "")
+set(CMAKE_OBJCOPY ${arm-toolchain-path}/bin/arm-none-eabi-objcopy
     CACHE FILEPATH "The toolchain objcopy command " FORCE)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
