0de18522e62191ee02a6d65192c69f51a99b26a5
a-int <somemailforicloud@icloud.com>
2023-07-28 10:02:52 +0300

    refactoring

--- example/flash.c
+++ example/flash.c
@@ -5,6 +5,7 @@
 volatile uint32_t delay_ms_time = 0;
 
 #define USART_RX_DATA_MAX 20
+
 struct USART {
   uint8_t rx_data[USART_RX_DATA_MAX];
   uint32_t rx_data_counter;
@@ -12,10 +13,10 @@ struct USART {
 struct USART huart2;
 
 int main() {
-  initSW(); // init serial wire 
-  init_clock_100(); //initialize MPU to work at 100 MHz
-  init_SysTick(1000); // initialize SysTick to work at ms
-  init_usart2_115200(); // initialize USART2 with Baudrate = 115200
+  initSW();              // init serial wire
+  init_clock_100();      //initialize MPU to work at 100 MHz
+  init_SysTick(1000);    // initialize SysTick to work at ms
+  init_usart2_115200();  // initialize USART2 with Baudrate = 115200
   while (1) {}
 }
 
@@ -31,16 +32,16 @@ void USART2_IRQHandler() {
       huart2.rx_data[huart2.rx_data_counter++] = (uint8_t)USART2->DR;
     }
     if (huart2.rx_data_counter == USART_RX_DATA_MAX) {  //the whole chunk was recieved
-      uint32_t* destination = (uint32_t*)0x8008000; //start from sector 3 (0x08008000)
-      __disable_irq(); //disable all irqs
+      uint32_t* destination = (uint32_t*)0x8008000;     //start from sector 3 (0x08008000)
+      __disable_irq();                                  //disable all irqs
       FLASH_unlock();
-      FLASH_clear_section(2); // sector 3: 0x0800_8000  -  0x0800_bfff
+      FLASH_clear_section(2);  // sector 3: 0x0800_8000  -  0x0800_bfff
       FLASH_write(huart2.rx_data, destination, huart2.rx_data_counter);
       FLASH_lock();
-      __enable_irq(); //enable irqs back
+      __enable_irq();  //enable irqs back
     }
   }
   if (READ_BIT(USART2->SR, USART_SR_IDLE)) {
-    USART2->DR; //reset IDLE flag
+    USART2->DR;  //reset IDLE flag
   }
 }
